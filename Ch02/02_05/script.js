b487b15b3b4dc2b5e8047c61ee83a5bff91d4a91
christopherdent <christopherdent01@gmail.com>
2021-07-31 22:54:42 -0400

    chap2 challenge1 complete

--- Ch02/02_05/script.js
+++ Ch02/02_05/script.js
@@ -4,5 +4,7 @@ jQuery(function(){
   let $group = $("#group-friends");
   $group.append('<input type="text" name="friends[]" value="" placeholder="Your friends name">');
 
+$group.find('input:last')
+      .remove()
 
 });
--- Ch02/02_06/script.js
+++ Ch02/02_06/script.js
@@ -1,4 +1,18 @@
 $(function(){
 'use strict';
-	
+
+	var $form = $('#my-form')
+
+	$form.find('.group:nth-child(1)').addClass('error')
+
+	$form.find('.group:nth-child(2)').addClass('msg').append('<p>Good choice!</p>')
+
 });
+
+//     var $grid = $('#bab_grid'),  //prefix JQ varaibles with $
+//     $lists = $grid.find('li');  //grab all list items inside bab_grid
+//
+// $lists.css('background', '#600')
+//   .animate({width : '-=100'}, 2000)
+//   .fadeOut()
+//   .fadeIn('slow')
