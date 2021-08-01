414d9325736a2750febb80f75ec12c3618f3d08c
christopherdent <christopherdent01@gmail.com>
2021-08-01 17:10:26 -0400

    Challenge 3 Complete

--- Ch03/03_03/script.js
+++ Ch03/03_03/script.js
@@ -1,9 +1,16 @@
-$(function() {
+$(document).ready(function() {
 'use strict';
 
 	$('#frmContact').submit(function(evt){
 		evt.preventDefault();
-		
-		
+		alert('ach ya clicked the thing!');
+
+		console.log("First Link", $('a:first').attr('href'))
+
+		$('#backpack').prop({
+			'checked' : false,
+			'disabled' : true
+		});
+
 	});
 });
--- Ch03/03_04/script.js
+++ Ch03/03_04/script.js
@@ -1,4 +1,35 @@
 $(function() {
 'use strict';
-	
+
+
+
+
+	var $checkboxes =
+		$('.checkbox-group').find('input[type=checkbox]');   //query the checkbox group by class but then find the checkbox inputs under it!
+			$checkboxes.prop('disabled', true)  //modify properties of the checkboxes to disable them
+
+			$('#bike_check').click(function(){
+				console.log('debug')
+				var $toggle = $(this);
+				console.log($toggle.prop('checked'))
+
+
+				if($toggle.prop('checked') === true) {
+					$checkboxes.prop('disabled', false);
+				} else {
+					$checkboxes.prop('disabled', true);
+				}
+	 		})
+
+
+	$('#newsletterNo').click(function() {
+console.log('you clicked no')
+		$('#frmContact').attr('action', 'contact-submitted-nosub.htm');
+	});
+
+	$('#newsletterYes').click(function() {
+		console.log('you clicked yes')
+		$('#frmContact').attr('action', 'contact-submitted.htm')
+	});
+
 });
