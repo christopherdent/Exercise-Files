834464352826a8fbe548baff57fa0741be0ae5d4
christopherdent <christopherdent01@gmail.com>
2021-07-31 22:34:14 -0400

    chap2 - apended fields

--- Ch02/02_05/script.js
+++ Ch02/02_05/script.js
@@ -1,4 +1,8 @@
-$(function(){
+jQuery(function(){
 'use strict';
+
+  let $group = $("#group-friends");
+  $group.append('<input type="text" name="friends[]" value="" placeholder="Your friends name">');
+
 
 });
